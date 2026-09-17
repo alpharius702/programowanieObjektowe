7f9caceb74c79cc60e9dfce6cd6de04d88586b4f
Bartosz Kyć <Horus10@gmail.com>
2026-09-17 11:33:56 +0200

    Zadania1

--- Czwiczenie01/src/Main.java
+++ Czwiczenie01/src/Main.java
@@ -1,34 +1,5 @@
 
 void main() {
-//
-//    System.out.println("hello World");
-//    System.out.println("Skrót"); //sout
-//
-//    System.out.print("Bez entera");
-//    System.out.println("Nowa Linia");
-//
-//    System.out.println("W takin sposób robić znaki specjalne");
-//    System.out.println("\t tabulacja");
-//    System.out.println("\n enter");
-//    System.out.println("To jest jak na maszynie \rdo pisania");
-//    System.out.println("\"cudzysłów\"");
-//    System.out.println("\'Apostrofa\'");
-//    System.out.println("backslash: \\");
-//
-//    int liczbaCalkowita = 5;
-//    double liczbaRzeczywista;
-//
-//    liczbaRzeczywista = (double) liczbaCalkowita /2;
-//    System.out.println("Wynik dzielenia liczby 5 przez 2 to " + liczbaRzeczywista);
-//
-//    char znak = 'a';
-//    System.out.println("Litera a ma wartość " + (znak + 0));
-//
-//    String napis = "napis";
-//    System.out.println("Napis Wielkimi literami " + napis.toUpperCase());
-//
-//    System.out.println("Wynik 1 + 2 + 3 = " + (1 + 2 + 3));
-//    System.out.println(1 + 2 + 3 + "To wynik 1+2+3");
-//
+    System.out.println("Bartosz");
 
 }
---
+++ Zad/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+//1
+    System.out.println("Ania");
+    System.out.println("Bartek");
+    System.out.println("Kasia");
+    //2
+    String imie = "Bartosz";
+    int rok = 2009;
+    double liczba = 0.66;
+    int rokobecny = 2026;
+    int wiek = rokobecny - rok;
+    //3
+    System.out.println("Mam na imie " + imie + "mam " + wiek + " lat i bede pisać mature za " + liczba);
+}

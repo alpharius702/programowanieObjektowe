fed2401efeeb8d3b9e2958c9f2f71ae019b2c036
Twoje Imię <email@email.com>
2026-09-23 13:34:37 +0200

    usuwam

--- Zad/src/Main.java
+++ Zad/src/Main.java
@@ -1,5 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
@@ -15,4 +16,12 @@ void main() {
     int wiek = rokobecny - rok;
     //3
     System.out.println("Mam na imie " + imie + "mam " + wiek + " lat i bede pisać mature za " + liczba);
+    //4
+    Scanner myObj = new Scanner(System.in);
+    System.out.println("Podaj mi liczbe Celcjuszy: ");
+
+    int cel = myObj.nextInt();
+    double fahr = 1.8*cel+10;
+    System.out.println("liczba w fahrenhajt: " + fahr);
+
 }

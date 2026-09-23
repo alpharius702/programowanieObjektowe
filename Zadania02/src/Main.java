3fe070bf4aeaaea912d5b0afc661f4e00b68258e
Twoje Imię <email@email.com>
2026-09-23 14:07:40 +0200

    Zadania

--- Zadania02/src/Main.java
+++ Zadania02/src/Main.java
@@ -1,13 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+    //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    Scanner scanner = new Scanner(system.in);
+    System.out.print("Podaj liczbę: ");
+    int liczba = scanner.nextInt();
+    if (liczba % 3 === 0) System.out.println("liczba jest podzilna przez 3");
+    else {
+        System.out.println("Liczba nie jest podzielna przez 3");
     }
 }

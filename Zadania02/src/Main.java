50de473f4781e685a31f934b521f5a64aa8a0bd7
Bartosz Kyc <Sanguison@gmail.com>
2026-09-24 11:33:14 +0200

    Zadania

--- Zadania02/src/Main.java
+++ Zadania02/src/Main.java
@@ -1,12 +1,26 @@
     //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import java.util.Scanner;
-void main() {
-    Scanner scanner = new Scanner(system.in);
-    System.out.print("Podaj liczbę: ");
-    int liczba = scanner.nextInt();
-    if (liczba % 3 === 0) System.out.println("liczba jest podzilna przez 3");
-    else {
-        System.out.println("Liczba nie jest podzielna przez 3");
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutte
+    import java.util.Scanner;
+
+    void main() {
+        //1
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj liczbę: ");
+        int liczba = scanner.nextInt();
+
+        if (liczba % 3 == 0) {
+            System.out.println("Liczba jest podzielna przez 3");
+        } else {
+            System.out.println("Liczba nie jest podzielna przez 3");
+        }
+
+        //2
+        System.out.print("Podaj Pierwszy bok Trójkąta: ");
+        int a = scanner.nextInt();
+        System.out.print("Podaj Drugiego bok Trójkąta: ");
+        int b = scanner.nextInt();
+        System.out.print("Podaj Trzeiego bok Trójkąta: ");
+        int c = scanner.nextInt();
+
+
     }
-}
